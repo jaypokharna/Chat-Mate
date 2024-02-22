35ed22f71adf3b6f9818b3953dd3822b5b774c42
jaypokharna <pokharnajay@gmail.com>
2024-02-22 18:55:08 +0530

    Ready for deployment

--- client/src/Components/MessageInput.jsx
+++ client/src/Components/MessageInput.jsx
@@ -2,42 +2,36 @@
 import React, { useState } from 'react'
 import { BsSend } from "react-icons/bs";
 import useSentMessage from '../Hooks/useSentMessage';
-import typingSound from "../assets/sounds/typing.mp3"
 
 
 const MessageInput = () => {
 
-const [message, setMessagee] = useState("")
+  const [message, setMessagee] = useState("")
 
-const {sendMessage,loading} = useSentMessage();
+  const { sendMessage, loading } = useSentMessage();
 
-const handleSubmit =async (e)=>{
-e.preventDefault()
+  const handleSubmit = async (e) => {
+    e.preventDefault()
 
-if(!message) {return}
+    if (!message) { return }
 
-await sendMessage(message)
-setMessagee('')
-}
+    await sendMessage(message)
+    setMessagee('')
+  }
 
-const typing = new Audio(typingSound);
 
-  
   return (
     <>
-    <form action="" className='px-4 my-3' onSubmit={handleSubmit}>
+      <form action="" className='px-4 my-3' onSubmit={handleSubmit}>
         <div className='w-full relative'>
-            <input type="text" className='border textsm rounded-lg block w-full p-2.5 bg-gray-700 border-gray-700 text-white pr-10' placeholder='...'
+          <input type="text" className='border textsm rounded-lg block w-full p-2.5 bg-gray-700 border-gray-700 text-white pr-10' placeholder='...'
             value={message}
-            onChange={(e)=>{
-              typing.play();
-              setMessagee(e.target.value)
-            }}/>
-            <button type='submit' className='absolute inset-y-0 end-0 flex items-center pe-3'>
+            onChange={(e) => setMessagee(e.target.value)} />
+          <button type='submit' className='absolute inset-y-0 end-0 flex items-center pe-3'>
             {loading ? <div className='loading loading-spinner'></div> : <BsSend />}
-            </button>
+          </button>
         </div>
-    </form>
+      </form>
     </>
   )
 }
--- server/server.js
+++ server/server.js
@@ -1,7 +1,8 @@
+import path from "path";
 import express from "express";
 import dotenv from "dotenv";
 import cors from "cors";
-import { app , server  } from "./socket/socket.js";
+import { app, server } from "./socket/socket.js";
 import authRoutes from "./routes/auth.routes.js";
 import messageRoutes from "./routes/message.routes.js";
 import userRoutes from "./routes/user.routes.js";
@@ -10,19 +11,29 @@ import cookieParser from "cookie-parser";
 
 const PORT = process.env.PORT || 5000;
 
+const __dirname = path.resolve();
+
 dotenv.config();
 app.use(express.json());
 app.use(cookieParser());
-app.use(cors({
-  origin: true,
-  credentials: true,
-  methods: ['GET', 'POST', 'PUT', 'DELETE'],
-  allowedHeaders: ['Content-Type', 'Authorization'],
-  exposedHeaders: ['Content-Length', 'Authorization'],
-}));
+app.use(
+  cors({
+    origin: true,
+    credentials: true,
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    allowedHeaders: ["Content-Type", "Authorization"],
+    exposedHeaders: ["Content-Length", "Authorization"],
+  })
+);
 app.use("/api/auth", authRoutes);
 app.use("/api/message", messageRoutes);
 app.use("/api/users", userRoutes);
+
+app.use(express.static(path.join(__dirname, "/client/dist")));
+
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "client", "dist", "index.html"));
+});
 
 app.get("/", (req, res) => {
   // root route http://localhost:3000
